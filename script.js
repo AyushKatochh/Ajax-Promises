d1b78d087a7f1afa5c8549dc7beadb6ea45a3eb5
Ayush Katoch <43515132+AyushKatochh@users.noreply.github.com>
2021-03-03 21:25:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,75 +25,8 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-/*
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
 
-  request.send();
-  console.log(request.responseText);
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    // Render country 1
-    renderCountry(data);
-
-    // Get neighbour country 2
-    const [neighbour] = data.borders;
-
-    if (!neighbour) return;
-
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-
-    request2.send();
-
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
-
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
-
-getCountryAndNeighbour('republic of india');
-
-setTimeout(() => {
-  console.log(`1 second passed`);
-  setTimeout(() => {
-    console.log(`2 second passed`);
-    setTimeout(() => {
-      console.log(`3 second passed`);
-
-      setTimeout(() => {
-        console.log(`4 second passed`);
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
-*/
-
-// const request = new XMLHttpRequest();
-// request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-
-// request.send();
-
-// const getCountryData = function (country) {
-//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-//     .then(function (response) {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       renderCountry(data[0]);
-//     });
-// };
 
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
